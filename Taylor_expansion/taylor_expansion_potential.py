28b795626ecb27840f8cc8d3cbe2049eb21c9cfd
Lauri Kinnunen <lauri.kinnuunen@gmail.com>
2026-02-17 10:47:10 +0200

    Update taylor_expansion_potential.py

--- Taylor_expansion/taylor_expansion_potential.py
+++ Taylor_expansion/taylor_expansion_potential.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math as m
+import numdifftools as nd
 
 # constants 
 λ = 1064*10**(-9)
@@ -13,11 +14,10 @@ p2 = 150*10**(-3)
 c = 3*10**8
 e0 = 9*10**(-12)
 ϵ = 1*10**(-12)
-r = 10**(-6)
+r = 200*10**(-6)
 mass = 1
-
-# define x_0
-x0 = 2.0
+pol = 1
+k = (2 * np.pi) / λ
 
 # Rayleigh range and field amplitude
 xR1 = (m.pi * w01**2 * n) / λ
@@ -25,54 +25,60 @@ xR2 = (m.pi * w02**2 * n) / λ
 P1 = (4 * p1) / (m.pi * w01**2 * c * e0)
 P2 = (4 * p2) / (m.pi * w02**2 * c * e0)
 
-# F = (A(x)^2)''+(A(-x)^2)''
-# function A is a product of three different functions (named f(x), g(x) and h(x)), for which I calculated the derivatives 
-# and then used the derivative of multiplication to tie the whole expression together
 
-f1 = (2 * x0 * xR1**2) / (x0**2 + xR1**2)**2
-f2 = (2 * x0 * xR2**2) / (x0**2 + xR2**2)**2
+# Function of the real field for the numerical solver
+def V(x_var):
+    return -0.25 * pol * (P1 * (1 + (x_var / xR1)**2)**(-1) * np.exp(-2 * r**2 / (w01**2 * (1 + (x_var / xR1)**2))) 
+                         + P2 * (1 + (x_var / xR2)**2)**(-1) * np.exp(-2 * r**2 / (w02**2 * (1 + (x_var / xR2)**2))))
 
-df1 = (2 * (x0**2 - xR1**2) * xR1**2 - 8 * x0**2 * xR1**2) / (x0**2 + xR1**2)**3
-df2 = (2 * (x0**2 - xR2**2) * xR2**2 - 8 * x0**2 * xR2**2) / (x0**2 + xR2**2)**3
+# x_0 from the roots of the potential first derivative
+# Root is r < w0
+x0 = 0
 
-ddf1 = -(8 * xR1**2 * x0) / (x0**2 + xR1**2)**5 - (16 * x0 * xR1**2 * (x0**2 + xR1**2) - 48 * x0**3 * xR1**2) / (x0**2 + xR1**2)**3
-ddf2 = -(8 * xR2**2 * x0) / (x0**2 + xR2**2)**5 - (16 * x0 * xR2**2 * (x0**2 + xR2**2) - 48 * x0**3 * xR2**2) / (x0**2 + xR2**2)**3
+# Roots if r > w0
+#pos_x01 = xR1 * np.sqrt((2 * r**2) / w01**2 - 1)
+#pos_x02 = xR2 * np.sqrt((2 * r**2) / w02**2 -1)
 
-g1 = 1 - (2 * r**2 * xR1**2) / (w01**2 * (x0**2 + xR1**2))
-g2 = 1 - (2 * r**2 * xR2**2) / (w02**2 * (x0**2 + xR2**2))
+#neg_x01 = -xR1 * np.sqrt((2 * r**2) / w01**2 - 1)
+#neg_x02 = -xR2 * np.sqrt((2 * r**2) / w02**2 - 1)
 
-dg1 = (8 * r**2 * xR1**2 * x0) / (w01**2 * (x0**2 + xR1**2)**2)
-dg2 = (8 * r**2 * xR2**2 * x0) / (w02**2 * (x0**2 + xR2**2)**2)
-
-ddg1 = (8 * r**2 * xR1**2 * (x0**2 + xR1**2) - 32 * r**2 * xR1**2 * x0**2) / (w01**2 * (x0**2 + xR1**2)**3)
-ddg2 = (8 * r**2 * xR2**2 * (x0**2 + xR2**2) - 32 * r**2 * xR2**2 * x0**2) / (w02**2 * (x0**2 + xR2**2)**3)
-
-h1 = m.exp(-(2 * r**2 * xR1**2) / (w01**2 * (x0**2 + xR1**2)))
-h2 = m.exp(-(2 * r**2 * xR2**2) / (w02**2 * (x0**2 + xR2**2)))
-
-dh1 = dg1 * h1
-dh2 = dg2 * h2
-
-ddh1 = ddg1 * h1 + dg1**2 * h1
-ddh2 = ddg2 * h2 + dg2**2 * h2
-
+# Interval for potential on x-axis around the minima
 x = np.linspace(x0 - 1, x0 + 1, 500)
-xtilde = (x - x0)*10**(-8) # Small interval for particle movement
+xtilde = (x - x0)
 
-dA1 = P1 * (ddf1 * g1 * h1 + df1 * dg1 * h1 + df1 * g1 * dh1 + ddg1 * f1 * h1 + dg1 * df1 * h1 + dg1**2 * f1 * h1
-                             + ddg1 * h1 * f1 * g1 + dg1**2 * h1 * f1 * g1 + dg1**2 * h1 * f1 + dg1 * h1 * g1 * df1)
-dA2 = P2 * (ddf2 * g2 * h2 + df2 * dg2 * h2 + df2 * g2 * dh2 + ddg2 * f2 * h2 + dg2 * df2 * h2 + dg2**2 * f2 * h2
-                             + ddg2 * h2 * f2 * g2 + dg2**2 * h2 * f2 * g2 + dg2**2 * h2 * f2 + dg2 * h2 * g2 * df2)
+# V = -0.5 * alpha * |E_tot(r,x)|^2 = -0.5 * alpha * (E(r, x)^2 + E(r, -x)^2)
+# Harmonic potential 
+# Second order derivative at x0 = 0, calculated analytically
+d1 = 2 / (xR1**2) * (2 * r**2 / w01**2 - 1) * np.exp(- (2 * r**2) / w01**2)
+d2 = 2 / (xR2**2) * (2 * r**2 / w02**2 - 1) * np.exp(- (2 * r**2) / w02**2)
 
-# Ω^2 = 1/m * ∂_x^ V
-Ohmega2 = (1 /  mass) * (dA1 + dA2)
+# Squared frequency Ω² = (1/m)∂²V
+ohmega2 = - 1 / (4 * mass) * pol * (P1 * d1 + P2 * d2)
 
-V = (1 / 2) * Ohmega2**2 * mass * xtilde**2
+# Analytically calculated harmonic potential V = (1/2)mΩ²x² plus constant term from taylor epansion
+V_harm = 0.5 * mass * ohmega2 * xtilde**2 + V(x0)
 
-plt.plot(xtilde, V)
+# Real field exponential factors
+exp_factor1 = np.exp(-1j * (k * xtilde + (k * r**2)/(2 * xtilde * (1 + (xR1 / xtilde)**2)) - np.arctan(xtilde/xR1)))
+exp_factor2 = np.exp(-1j * (- k * xtilde + (k * r**2)/(- 2 * xtilde * (1 + (xR2 / xtilde)**2)) - np.arctan(-xtilde/xR2)))
+
+# Anharmonic potential analytical form
+V_nharm = -0.25 * pol * (P1 * (1 + (xtilde / xR1)**2)**(-1) * np.exp(-2 * r**2 / (w01**2 * (1 + (xtilde / xR1)**2))) 
+                         + P2 * (1 + (xtilde / xR2)**2)**(-1) * np.exp(-2 * r**2 / (w02**2 * (1 + (xtilde / xR2)**2))))
+
+# Numerical solution for reference
+d2f = nd.Derivative(V, n = 2)
+d2f_at_x0 = d2f(x0)
+V_harm_num = 0.5 * d2f_at_x0 * xtilde**2 + V(x0)
+
+# Plots
+plt.plot(xtilde, V_nharm, label = 'V anharm')
+plt.plot(xtilde, V_harm, label = 'V harm')
+plt.plot(xtilde, V_harm_num, 'b--', label = 'V harm num', linewidth = 0.5)
+plt.legend()
 plt.xlabel(r'$\tilde{x} = x - x_0$')
 plt.ylabel('Potential')
-plt.title('Potential form the fields Taylor expansions second term')
 plt.show()
+print(min(V_harm))
 
 
